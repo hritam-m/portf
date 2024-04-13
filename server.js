27683bbf317fcd5ec90df7022c6b7b2aa2aff764
Hritam Mondal <hritamcs@outlook.com>
2024-04-13 21:58:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 // Define the directory where your HTML files are located
 const publicDirectoryPath = path.join(__dirname, 'src');
